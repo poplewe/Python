27f704a7efefb23d80d84e8a54d32c6e01baf059
poplewe <poplewe@gmail.com>
2020-08-16 22:46:48 +0900

    Create 2.Pandas Basic.py

---
+++ 2.Pandas Basic.py
@@ -0,0 +1,19 @@
+
+
+import pandas as pd
+
+
+
+
+temp_series=pd.Series([['A','B','C'],['a','b','c'],['apple','bus','cat']]);
+print(temp_series.head())
+
+
+print(type(temp_series))
+print(temp_series.shape)
+
+print(type(temp_series.iloc[0]))
+
+
+
+
