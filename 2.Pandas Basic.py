1671872ae01ff7e3cba0e558ae3b7b8213d3fd2c
poplewe <poplewe@gmail.com>
2020-08-19 21:58:21 +0900

    Update 2.Pandas Basic.py

--- 2.Pandas Basic.py
+++ 2.Pandas Basic.py
@@ -1,7 +1,7 @@
 
 
 import pandas as pd
-
+import numpy as np
 
 
 
@@ -17,3 +17,11 @@ print(type(temp_series.iloc[0]))
 
 print(temp_series.loc[1])
 
+data_arr=np.random.random((90,5))
+print(data_arr.shape)
+data_col=['A','B','C','D','E']
+print(data_col)
+
+df_data=pd.DataFrame(data_arr,columns=data_col);
+print(df_data.head())
+print(df_data.shape)
